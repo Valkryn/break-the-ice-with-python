ef2ecf79065024291d978a9d91c59fb0b2ccf1d8
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-11-05 15:08:45 -0500

    alternate answer using filter

--- Day_1/q1.py
+++ Day_1/q1.py
@@ -1,18 +1,28 @@
 # Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
 # between 2000 and 3200 (both included).The numbers obtained should be printed in a comma-separated sequence on a single line.
 
+# def main():
+#     numbers = range(2000, 3200)
+#     filtered_numbers = filter_numbers(numbers)
+#     print(filtered_numbers)
+#
+#
+# def filter_numbers(lst):
+#     filtered_numbers = []
+#     for number in lst:
+#         if number % 7 == 0 and number % 5 != 0:
+#             filtered_numbers.append(number)
+#     return filtered_numbers
+#
+#
 def main():
-    numbers = range(2000, 3200)
-    filtered_numbers = filter_numbers(numbers)
-    print(filtered_numbers)
+    numbers = list(range(2000, 3200))
+    lst = list(filter(filter_range, numbers))
+    print(lst)
 
 
-def filter_numbers(lst):
-    filtered_numbers = []
-    for number in lst:
-        if number % 7 == 0 and number % 5 != 0:
-            filtered_numbers.append(number)
-    return filtered_numbers
+def filter_range(num):
+    return num % 7 == 0 and num % 5 != 0
 
 
 if __name__ == '__main__':
